e342390177eeb815d09ccb90341b1fc149a252e9
Nitesh47kumars <niteshshah028@gmail.com>
2025-08-02 15:09:51 +0530

    Add OVerlay In HeroSection

--- src/Components/HeroSection/HeroImg.jsx
+++ src/Components/HeroSection/HeroImg.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-
-const HeroImg = () => {
+import { TbCircleDashed } from "react-icons/tb";const HeroImg = () => {
   return (
-    <div>
-        <div className='rounded-full overflow-hidden flex justify-center bg-red-300'>
-            <img className='max-h-70 max-w-70' src='/Pic.jpg' alt=''/>
+    <div className='flex justify-center items-center relative'>
+        <div className='rounded-full overflow-hidden flex justify-center bg-red-300 w-70 h-70'>
+            <img className='h-full w-full' src='/Pic.jpg' alt=''/>
         </div>
+        <div className='absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 -z-10 w-72 h-72 bg-gradient-to-r from-red-600 to-blue-600 rounded-full blur-sm'></div>
     </div>
   )
 }
--- src/Components/HeroSection/HeroOverlay.jsx
+++ src/Components/HeroSection/HeroOverlay.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const HeroOverlay = () => {
   return (
     <div>
-      <div className='absolute h-32 w-32 top-0 right-10 bg-red-600 -z-10'></div>
+      <div className='absolute h-40 w-40 top-0 right-0 bg-[#FF0000] rounded-full blur-[5rem] -z-10 animate-pulse'></div>
+      <div className='absolute h-40 w-40 top-0 right-[14rem] bg-[#ff9d00] rounded-full blur-[5rem] -z-10 animate-pulse'></div>
+      <div className='absolute h-60 w-60 top-60 left-[5rem] bg-[#ff00008d] rounded-full blur-[5rem] -z-10'></div>
+      <div className='absolute h-80 w-80 top-120 left-[-6rem] bg-[#ff9d0088] rounded-full blur-[5rem] -z-10'></div>
     </div>
   )
 }
