60dac63c6b0339dbd98cc0b4c66ef8dcf9ef4ed6
Nitesh47kumars <niteshshah028@gmail.com>
2025-08-05 00:12:16 +0530

    Add Motion to AboutContent

--- src/Components/About/AboutContent.jsx
+++ src/Components/About/AboutContent.jsx
@@ -1,22 +1,42 @@
 import React from 'react'
+import {motion} from "motion/react"
+import { animation } from '../../FramerMotion/Animation';
 
 const AboutContent = () => {
   return (
-    <div className='flex flex-col max-lg:w-full lg:w-[60%] max-md:w-130 md:tracking-widest
+    <div
+    className='flex flex-col max-lg:w-full lg:w-[60%] max-md:w-130 md:tracking-widest
         max-lg:items-center'>
-        <h1 className='max-lg:text-center'>About Me!</h1>
+        <motion.h1
+        variants={animation("up",0.2)}
+        initial={"hidden"}
+        whileInView={"show"}
+        viewport={{one:false,amout:0}}
+        className='max-lg:text-center'>About Me!</motion.h1>
             <br/>
         <div className='flex flex-col gap-5 max-lg:text-center md:leading-8
             max-sm:w-[90vw] max-sm:text-[clamp(10px,2vw,15px)]'>
-            <p>
+            <motion.p
+                variants={animation("up",0.3)}
+                initial={"hidden"}
+                whileInView={"show"}
+                viewport={{one:false,amout:0}}>
                 Hi! I’m Nitesh Kumar, a 19-year-old passionate web developer and video editor. Currently, I’m a second-year BCA student, diving deep into the world of coding and design. I have hands-on experience with HTML, CSS, JavaScript, React, and Tailwind CSS, and I love creating clean, responsive, and user-friendly websites.
-            </p>
-            <p>
+            </motion.p>
+            <motion.p
+                variants={animation("up",0.4)}
+                initial={"hidden"}
+                whileInView={"show"}
+                viewport={{one:false,amout:0}}>
                 Alongside web development, I also enjoy video editing, which helps me bring creativity and storytelling into my projects. This portfolio is my first step to showcase my skills, projects, and journey as I grow in the tech world.
-            </p>
-            <p>
+            </motion.p>
+            <motion.p
+                variants={animation("up",0.5)}
+                initial={"hidden"}
+                whileInView={"show"}
+                viewport={{one:false,amout:0}}>
                 I’m excited to connect, learn, and create meaningful digital experiences. Let’s build something amazing together!
-            </p>
+            </motion.p>
         </div>
     </div>
   )
