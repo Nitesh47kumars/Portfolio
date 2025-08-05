03fcbf77f409e7dde226a72ab367cbcfabbbfb39
Nitesh47kumars <niteshshah028@gmail.com>
2025-08-05 23:38:48 +0530

    Add Stucture of About Image

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
   return (
     <div
     id='about'
-    className='relative max-md:mt-70 md:mt-110 pb-40 w-full flex justify-center items-center'>
+    className='relative max-md:mt-70 md:mt-110 md:pb-20 w-full flex justify-center items-center'>
         <div className='w-[80%] flex lg:flex-row max-lg:flex-col lg:justify-between items-center'>
             <AboutContent/>
             <AboutImage/>
--- src/Components/About/AboutImage.jsx
+++ src/Components/About/AboutImage.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const AboutImage = () => {
   return (
-    <div>
-      <h1>Images....</h1>
+    <div className='overflow-hidden lg:w-[40%] relative'>
+      <div className='lg:w-[100%] lg:h-100 object-cover overflow-hidden'>
+        <img src='/Picture.jpg' alt=''/>
+      </div>
+      <div className='absolute top-0 right-0 bg-gradient-to-r from-black to-transparent'/>
     </div>
   )
 }
