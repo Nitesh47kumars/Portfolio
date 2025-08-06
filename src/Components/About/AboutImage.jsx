d4034c11896df77813b2825dffdbcbca0e7ffc5e
Nitesh47kumars <niteshshah028@gmail.com>
2025-08-06 21:50:41 +0530

    Add Video to AboutImage Component

--- src/Components/About/AboutImage.jsx
+++ src/Components/About/AboutImage.jsx
@@ -5,7 +5,12 @@ const AboutImage = () => {
     <div className='overflow-hidden lg:w-[40%] relative'>
       <div className='lg:w-[100%] lg:h-100 max-lg:h-150 object-cover overflow-hidden
       max-lg:translate-y-[-10rem] '>
-        <img className='rounded-xl' src='/Picture.jpg' alt=''/>
+        <video src='/video.mp4'
+          loop
+          muted
+          autoPlay
+          playsInline
+          className='h-full w-full object-cover rounded-2xl'/>
       </div>
       <div className='absolute top-0 right-0 h-full w-full max-lg:bg-gradient-to-b lg:bg-gradient-to-r from-black to-transparent'/>
     </div>
