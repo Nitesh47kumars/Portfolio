67685e816cdf8f6c0f8ff999d9d1c70c0f5ac23b
Nitesh47kumars <niteshshah028@gmail.com>
2025-08-06 20:26:46 +0530

    Set About Image Position

--- src/Components/About/AboutImage.jsx
+++ src/Components/About/AboutImage.jsx
@@ -3,10 +3,11 @@ import React from 'react'
 const AboutImage = () => {
   return (
     <div className='overflow-hidden lg:w-[40%] relative'>
-      <div className='lg:w-[100%] lg:h-100 object-cover overflow-hidden'>
-        <img src='/Picture.jpg' alt=''/>
+      <div className='lg:w-[100%] lg:h-100 max-lg:h-150 object-cover overflow-hidden
+      max-lg:translate-y-[-10rem] '>
+        <img className='rounded-xl' src='/Picture.jpg' alt=''/>
       </div>
-      <div className='absolute top-0 right-0 bg-gradient-to-r from-black to-transparent'/>
+      <div className='absolute top-0 right-0 h-full w-full max-lg:bg-gradient-to-b lg:bg-gradient-to-r from-black to-transparent'/>
     </div>
   )
 }
