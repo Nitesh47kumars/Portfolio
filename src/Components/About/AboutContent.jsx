fd8f51f57007a2d6270a12f51f23f140c1c30e60
Nitesh47kumars <niteshshah028@gmail.com>
2025-08-06 20:30:32 +0530

    Change About Content Animation

--- src/Components/About/AboutContent.jsx
+++ src/Components/About/AboutContent.jsx
@@ -8,7 +8,7 @@ const AboutContent = () => {
     className='flex flex-col max-lg:w-full lg:w-[60%] max-md:w-130 md:tracking-widest
         max-lg:items-center'>
         <motion.h1
-        variants={animation("up",0.2)}
+        variants={animation("down",0.2)}
         initial={"hidden"}
         whileInView={"show"}
         viewport={{one:false,amout:0}}
@@ -17,21 +17,21 @@ const AboutContent = () => {
         <div className='flex flex-col gap-5 max-lg:text-center md:leading-8
             max-sm:w-[90vw] max-sm:text-[clamp(10px,2vw,15px)]'>
             <motion.p
-                variants={animation("up",0.3)}
+                variants={animation("right",0.3)}
                 initial={"hidden"}
                 whileInView={"show"}
                 viewport={{one:false,amout:0}}>
                 Hi! I’m Nitesh Kumar, a 19-year-old passionate web developer and video editor. Currently, I’m a second-year BCA student, diving deep into the world of coding and design. I have hands-on experience with HTML, CSS, JavaScript, React, and Tailwind CSS, and I love creating clean, responsive, and user-friendly websites.
             </motion.p>
             <motion.p
-                variants={animation("up",0.4)}
+                variants={animation("right",0.4)}
                 initial={"hidden"}
                 whileInView={"show"}
                 viewport={{one:false,amout:0}}>
                 Alongside web development, I also enjoy video editing, which helps me bring creativity and storytelling into my projects. This portfolio is my first step to showcase my skills, projects, and journey as I grow in the tech world.
             </motion.p>
             <motion.p
-                variants={animation("up",0.5)}
+                variants={animation("right",0.5)}
                 initial={"hidden"}
                 whileInView={"show"}
                 viewport={{one:false,amout:0}}>
