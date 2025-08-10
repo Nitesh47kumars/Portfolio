c534ba95f83b9fe31815ba8d4888f61f124d072c
Nitesh47kumars <niteshshah028@gmail.com>
2025-08-10 23:38:39 +0530

    Fix Navbar Button Size

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
     <div
     id='about'
     className='relative max-md:mt-70 md:mt-110 md:pb-30 w-full flex justify-center items-center'>
-        <div className='w-[80%] flex max-sm:gap-5 lg:flex-row max-lg:flex-col lg:justify-between items-center'>
+        <div className='w-[80%] flex max-md:gap-5 lg:flex-row max-lg:flex-col lg:justify-between items-center'>
             <AboutContent/>
             <AboutImage/>
         </div>
--- src/Components/Navbar/NavbarBtn.jsx
+++ src/Components/Navbar/NavbarBtn.jsx
@@ -3,8 +3,9 @@ import { FaArrowRight } from "react-icons/fa";
 
 const NavbarBtn = () => {
   return (
-    <div className="">
-      <button className="flex items-center gap-1 font-semibold py-2 px-4 border-1 rounded-full bg-gradient-to-r from-[#8e2de2] to-[#ff6a00] hover:scale-105 transition-all duration-300 ">
+    <div>
+      <button className="flex items-center gap-1 font-bold py-2 px-4 border-1 rounded-full bg-gradient-to-r from-[#8e2de2] to-[#ff6a00] hover:scale-105 transition-all duration-300
+        max-md:text-sm md:text-md max-sm:px-3">
         Hire Me
         <FaArrowRight />
       </button>
