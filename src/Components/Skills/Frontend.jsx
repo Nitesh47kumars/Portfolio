1ec3edb16e8aa66be1eae60ff3444dd52c1b14bf
Nitesh47kumars <niteshshah028@gmail.com>
2025-08-12 23:52:03 +0530

    Forget to Add The Animarion

--- src/Components/Skills/Frontend.jsx
+++ src/Components/Skills/Frontend.jsx
@@ -8,7 +8,7 @@ const Frontend = () => {
     '/React-icon.svg.png',
   ]
   return (
-    <div className='col-span-2 h-[300px] rounded-xl bg-black transition-all duration-500
+    <div className='col-span-2 h-[300px] rounded-xl bg-black transition-all duration-500 group
     shadow-[inset_6px_6px_60px_rgba(225,225,225,0.2)] hover:shadow-[inset_0_0px_80px_rgba(255,255,255,0.2)]'>
       <h1 className='text-center pt-10 text-white text-xl'>Frontend...</h1>
 
@@ -16,7 +16,7 @@ const Frontend = () => {
         {images.map((curr,idx)=>{
           return <li
           key={idx}
-          className="h-28 w-28 rounded-[20px] border-2 p-2 transition-all duration-300 md:group-hover:-translate-y-3 md:group-hover:border-indigo-400
+          className="h-28 w-28 rounded-[20px] border-2 p-2 transition-all duration-500 md:group-hover:-translate-y-2 md:group-hover:border-indigo-400
             hover:border-blue-600">
               <div className="grid h-full place-items-center rounded-xl border-2 border-[#A5AEB81F]/10 bg-[#EDEEF0] dark:border-[#5A5F661F]/10 dark:bg-[#1A1B1E]"
                 style={{'box-shadow':'rgba(165, 174, 184, 0.32) 0px 2px 1.5px 0px inset'}}>
