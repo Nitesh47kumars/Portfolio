8ddae142a61881270f0073f824e24c1513868fd3
Nitesh47kumars <niteshshah028@gmail.com>
2025-08-16 21:59:17 +0530

    Fix Styling Issues in HeroSection

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -4,7 +4,7 @@ import HeroImg from './HeroImg'
 
 const HeroSection = () => {
   return (
-    <div className='w-full mt-40 max-md:mt-30 flex justify-center items-center overflow-hidden py-5'>
+    <div id='home' className='w-full pt-40 max-md:pt-30 flex justify-center items-center pb-5 overflow-hidden'>
       <div className='w-[80%] h-auto md:h-[20rem] flex justify-between items-center md:flex-row max-md:flex-col max-sm:gap-10 max-md:gap-6'>
         <HeroContent/>
         <HeroImg/>
