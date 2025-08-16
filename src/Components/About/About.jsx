714e9d603978bb46107f6ffe528804f28c2af712
Nitesh47kumars <niteshshah028@gmail.com>
2025-08-16 21:26:23 +0530

    Fix Margin & Padding in About Component

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
   return (
     <div
     id='about'
-    className='relative max-md:mt-70 md:mt-110 md:pb-30 w-full flex justify-center items-center'>
+    className='relative max-md:mt-70 md:mt-80 md:py-30 w-full flex justify-center items-center'>
         <div className='w-[80%] flex max-md:gap-5 lg:flex-row max-lg:flex-col lg:justify-between items-center'>
             <AboutContent/>
             <AboutImage/>
