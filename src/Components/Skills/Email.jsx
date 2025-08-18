ddd0f7ad92cebd3f5c16a022b8323d581be41e8a
Nitesh47kumars <niteshshah028@gmail.com>
2025-08-18 23:05:33 +0530

    Change Email Section background Image

--- src/Components/Skills/Email.jsx
+++ src/Components/Skills/Email.jsx
@@ -16,7 +16,7 @@ const Email = () => {
       <div className="absolute top-0 left-0 z-[-1] inset-0">
         <img
           className="h-full w-full blur-[1px]"
-          src="/premium_photo-1686309673130-36e6a28333a3.avif"
+          src="https://plus.unsplash.com/premium_photo-1681400019731-5d7cc4cafb9d?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         />
       </div>
       <div className="h-full flex flex-col items-center justify-around">
