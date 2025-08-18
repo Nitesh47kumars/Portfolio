ed85f64b5a77700cd149e83a0b89222d23a749eb
Nitesh47kumars <niteshshah028@gmail.com>
2025-08-18 10:06:15 +0530

    Start Working on SoftSkill section & add Pre-define Component

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import Motion from './Motion'
 import Tools from './Tools'
 import Hobbies from './Hobbies'
 import Email from './Email'
 import Frontend from './Frontend'
+import SoftSkills from './SoftSkills'
 
 const Skills = () => {
   return (
     <div id='skills' className=' text-black w-full flex justify-center items-center'>
         <div className='w-[80%] grid grid-cols-3 gap-5 my-20'>
             <Frontend/>
-            <Motion/>
+            <SoftSkills/>
             <Hobbies/>
             <Email/>
             <Tools/>
--- src/Components/Skills/SoftSkills.jsx
+++ src/Components/Skills/SoftSkills.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import {motion} from 'motion/react'
 import {animation} from '../../Motions/Animation'
+import Card from '../../UI/Card'
 
-const Motion = () => {
+const SoftSkills = () => {
   return (
     <motion.div
       variants={animation("right",0.2)}
@@ -11,9 +12,11 @@ const Motion = () => {
       viewport={{once:false, amount:0}}
       className='row-span-2 h-full rounded
       shadow-[inset_-6px_6px_60px_rgba(225,225,225,0.2)] hover:shadow-[inset_0_0px_80px_rgba(255,255,255,0.2)]'>
-        <h1>Motion...</h1>
+        <h1>Soft Skills</h1>
+
+      <Card/>
     </motion.div>
   )
 }
 
-export default Motion
+export default SoftSkills
