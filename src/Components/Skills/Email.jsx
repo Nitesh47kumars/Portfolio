b9674f1687347f268de060509ba21a6747feb8a7
Nitesh47kumars <niteshshah028@gmail.com>
2025-08-24 22:31:42 +0530

    Make Email Section in Skills Component Responsive

--- src/Components/Skills/Email.jsx
+++ src/Components/Skills/Email.jsx
@@ -22,19 +22,19 @@ const Email = () => {
       initial={"hidden"}
       whileInView={"show"}
       viewport={{ once: true, amount: 0 }}
-      className="md:col-span-2 h-[200px] w-full rounded relative transition-shadow duration-500
+      className="md:col-span-2 h-[200px] max-md:h-[150px] w-full rounded relative transition-shadow duration-500
       shadow-[inset_0_0_20px_rgba(0,0,0,0.3)] hover:shadow-[inset_0_0px_80px_rgba(0,0,0,0.6)] overflow-hidden"
     >
       <div className="absolute top-0 left-0 z-[-1] ">
         <img className="h-full w-full blur-[1px]" src="/Email-bg.avif" />
       </div>
-      <div className="h-full flex flex-col items-center justify-around">
+      <div className="h-full flex flex-col items-center justify-around text-center">
         <motion.h1
           variants={animation("", 0.6)}
           initial={"hidden"}
           whileInView={"show"}
           viewport={{ once: true, amount: 0 }}
-          className="text-white w-[90%] text-[1.3rem]"
+          className="text-white w-[90%] max-md:text-[14px] xl:text-xl"
           style={{ textShadow: "1px 1px 1px rgba(0, 0, 0, 0.8)" }}
         >
           Feel free to reach out for collaborations, freelance work, or just to
@@ -50,7 +50,7 @@ const Email = () => {
           viewport={{ once: true, amount: 0 }}
           type="button"
           style={{ textShadow: "1px 1px 1px rgba(0, 0, 0, 0.8)" }}
-          className="items-center gap-2 py-3 text-base text-black dark:text-white/75 outline-hidden transition-all duration-500 cursor-pointer hover:text-black/60 dark:hover:text-white/100 flex w-[80%] justify-center rounded-md bg-white/10 px-8 shadow-[inset_0_3px_10px_#ffffff3f]
+          className="items-center gap-2 py-3 text-[clamp(0.75rem,1vw,1rem)] text-black dark:text-white/75 outline-hidden transition-all duration-500 cursor-pointer hover:text-black/60 dark:hover:text-white/100 flex w-[80%] justify-center rounded-md bg-white/10 shadow-[inset_0_3px_10px_#ffffff3f]
           backdrop-blur-[1px] font-bold tracking-wide text-shadow-lg"
         >
           {copy ? <Check /> : <Copy controls={controls}/>}
