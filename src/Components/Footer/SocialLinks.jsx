2da0355652693f911c84b1a306d4eb5398407562
Nitesh47kumars <niteshshah028@gmail.com>
2025-08-28 17:11:05 +0530

    Add Icons in Connect Section in Footer Component

--- src/Components/Footer/SocialLinks.jsx
+++ src/Components/Footer/SocialLinks.jsx
@@ -3,18 +3,18 @@ import React from "react";
 const links = [
   {
     name: "GitHub",
-    href: "https://github.com/The-Abhishek-Singh",
-    icon: "🐱",
+    href: "https://github.com/Nitesh47kumars",
+    icon: "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/github-white-icon.png",
   },
   {
     name: "LinkedIn",
-    href: "https://www.linkedin.com/in/abhishekdata100/",
-    icon: "💼",
+    href: "https://www.linkedin.com/in/nitesh-kumar-b18348346/",
+    icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/960px-LinkedIn_logo_initials.png",
   },
   {
     name: "Twitter",
-    href: "https://x.com/Abhishek_Data_",
-    icon: "🐦",
+    href: "https://x.com/Niteshshah028",
+    icon: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZpeZi0IQdFiMpt_ljY6l5hsUrqLB2Qie_Ug&s",
   },
 ];
 
@@ -31,9 +31,9 @@ const SocialLinks = () => (
           className="group relative"
         >
           <div className="relative">
-            <span className="text-gray-400 transition-all duration-300 ease-out group-hover:text-white group-hover:scale-105">
-              {icon}
-            </span>
+            <div className="w-8 h-8 object-cover rounded-full overflow-hidden">
+              <img className="w-full h-full" src={icon} alt={name} />
+            </div>
             <span className="absolute -bottom-6 left-1/2 transform -translate-x-1/2 text-xs text-gray-400 opacity-0 transition-all duration-300 ease-out group-hover:opacity-100 group-hover:text-white whitespace-nowrap">
               {name}
             </span>
