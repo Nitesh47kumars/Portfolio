efb76f680891e0374046cdaa122c5b4ae86e3ded
Nitesh47kumars <niteshshah028@gmail.com>
2025-08-28 17:18:44 +0530

    Fix & update Contact Info Section in Footer Comonent

--- src/Components/Footer/ContactInfo.jsx
+++ src/Components/Footer/ContactInfo.jsx
@@ -5,14 +5,14 @@ const ContactInfo = () => (
     <h4 className="font-semibold">Get in Touch</h4>
     <div className="space-y-3">
       <a
-        href="mailto:abhi120730@gmail.com"
+        href="mailto:niteshshah028@gmail.com"
         className="text-gray-400 hover:text-white transition-all duration-300 ease-out text-sm flex items-center group"
       >
         <span className="transform transition-all duration-300 ease-out group-hover:translate-x-1">
           📧
         </span>
         <span className="ml-2 relative">
-          abhi120730@gmail.com
+          niteshshah028@gmail.com
           <span className="absolute -bottom-0.5 left-0 w-0 h-px bg-white transition-all duration-300 ease-out group-hover:w-full"></span>
         </span>
       </a>
@@ -21,7 +21,7 @@ const ContactInfo = () => (
         <span className="transform transition-all duration-300 ease-out group-hover:translate-x-1">
           📍
         </span>
-        <span className="ml-2">Raipur, Chhattisgarh, India</span>
+        <span className="ml-2">Yamuna-nagar, Haryana, India.</span>
       </div>
     </div>
   </section>
