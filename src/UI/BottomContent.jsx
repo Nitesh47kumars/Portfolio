bf0a74ca199a4466a2f11580b5ef9e783beea0d8
Nitesh47kumars <niteshshah028@gmail.com>
2025-08-29 17:34:10 +0530

    Trying to Add Background Video in BottomContent

--- src/UI/BottomContent.jsx
+++ src/UI/BottomContent.jsx
@@ -5,6 +5,12 @@ import Footer from '../Components/Footer/Footer'
 const BottomContent = () => {
   return (
     <div className='max-md:h-[1180px] md:h-[650px]'>
+      <video
+      className='w-full h-full z-[-2]'
+      loop
+      autoPlay
+      src='https://cdn.pixabay.com/video/2021/03/06/67139-521253317_large.mp4'
+      />
       <BlackHole/>
       <Footer/>
     </div>
