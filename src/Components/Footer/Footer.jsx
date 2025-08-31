8d4a19396c17c9e99f82c70bed9423340684c618
Nitesh47kumars <niteshshah028@gmail.com>
2025-08-31 12:21:10 +0530

    Making Project Section Responsive

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ import FooterBottom from "./FooterBottom";
 
 const Footer = () => {
   return (
-    <footer className="lg:mt-30 text-white font-openSans">
+    <footer id="contact" className="lg:mt-30 text-white font-openSans">
       <div className="mx-auto px-6 bg-white/5 p-7 rounded-2xl backdrop-blur-[3px]
       lg:w-[80%] max-lg:w-[80%] max-md:w-[90%] max-sm:w-full max-sm:rounded-none">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-12 sm:gap-24 mb-12">
--- src/Components/Projects/Project.jsx
+++ src/Components/Projects/Project.jsx
@@ -3,42 +3,53 @@ import ProjectData from "./ProjectData.json";
 
 const Project = () => {
   return (
-    <div className="w-full place-items-center">
-      <div className="w-[80%] flex justify-around gap-10">
-        <div className="w-[60%] min-h-[500px] border border-white rounded-3xl">
-          <div className="h-full w-full bg-orange-400 border-8 border-[#3d3737] rounded-3xl">
-            <img />
+    <div className="w-full max-md:mt-30 place-items-center">
+      <div className="w-[95%] xl:w-[80%] flex justify-around gap-10 max-md:flex-col">
+        <div className="md:w-[60%] lg:h-[600px] border border-white/50 rounded-3xl">
+          <div className="h-full w-full bg-orange-400 border-8 border-[#292525] rounded-3xl
+            flex justify-center items-end group overflow-hidden">
+            <img
+              alt="Snippix"
+              loading="lazy"
+              src="./Project/Project.jpg"
+              className="h-[clamp(20rem,2vw,25rem)] w-full max-w-[85%] object-cover lg:group-hover:translsate-y-10 translate-y-5 -rotate-3 rounded-t-lg border-[1.5px] border-white/20 transition-all duration-300 will-change-transform 
+              lg:block lg:rotate-0 lg:group-hover:scale-[1.08] lg:group-hover:-rotate-3 shadow-[0_0_30px_#14B8A6]"
+            />
           </div>
         </div>
           
-        <div className="w-[40%]">
+        <div className="md:w-[40%]">
           {ProjectData.map((curr, idx) => {
             const { title, description, features, technologies} = curr;
             return (
               <li key={idx} className="list-none">
                 <h1 className="text-2xl font-bold">{title}</h1>
-                <p className="text-white/50 my-3">{description}</p>
+                <p className="text-white/70 my-3">{description}</p>
                 <ul>
-                  {features.map((curr) => {
+                  {features.map((curr,idx) => {
                     return (
-                      <div className="flex items-center justify-center">
+                      <div key={idx} className="max-lg:hidden flex items-center justify-center">
                         <svg
                           width="24"
                           height="24"
                           viewBox="0 0 24 24"
                           xmlns="http://www.w3.org/2000/svg"
-                          class="mt-1 mr-2 size-5 shrink-0 fill-teal-600 text-teal-600 dark:text-teal-400 bg-teal-600/20 lg:bg-white-1 dark:lg:bg-black"
+                          className="mt-1 mr-2 size-5 shrink-0 fill-teal-600 text-teal-600 dark:text-teal-400 bg-teal-600/20 lg:bg-white-1 dark:lg:bg-black"
                         >
                           <path d="M12 1C12 1 12 8 10 10C8 12 1 12 1 12C1 12 8 12 10 14C12 16 12 23 12 23C12 23 12 16 14 14C16 12 23 12 23 12C23 12 16 12 14 10C12 8 12 1 12 1Z"></path>
                         </svg>
-                        {curr}
+                        <p
+                        className="text-[16px]">
+                          {curr}
+                        </p>
                       </div>
                     );
                   })}
                 </ul>
                 <div className="w-[70%] flex flex-wrap gap-3 mt-10 place-items-start">
-                  {technologies.map((curr)=>{
+                  {technologies.map((curr,idx)=>{
                     return <div
+                    key={idx}
                     className="h-8 min-w-22 border px-3 rounded-xl text-nowrap bg-neutral-900 flex items-center justify-center">
                       {curr}
                     </div>
