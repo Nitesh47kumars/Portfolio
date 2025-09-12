d8992fb93dc0fbb387d22134279acd8f40259345
Nitesh47kumars <niteshshah028@gmail.com>
2025-09-12 20:49:43 +0530

    Add Functionality to download Resume in NavbarBtn

--- src/Components/Navbar/NavbarBtn.jsx
+++ src/Components/Navbar/NavbarBtn.jsx
@@ -5,17 +5,15 @@ const NavbarBtn = () => {
   const [isHovered, setIsHovered] = useState(false);
 
   return (
-    <div>
-      <button
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
-        className="flex items-center gap-1 font-bold py-2 px-4 border-1 rounded-full bg-gradient-to-r from-[#8e2de2] to-[#ff6a00] hover:scale-105 transition-all duration-300
-          max-md:text-sm md:text-md max-sm:px-3"
-      >
-        Resume
-        <Download isHovered={isHovered} />
-      </button>
-    </div>
+    <a href="/Resume.pdf" download
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      className="flex items-center gap-1 font-bold py-2 px-4 border-1 rounded-full bg-gradient-to-r from-[#8e2de2] to-[#ff6a00] hover:scale-105 transition-all duration-300
+        max-md:text-sm md:text-md max-sm:px-3"
+    >
+      Resume
+      <Download isHovered={isHovered} />
+    </a>
   );
 };
 
