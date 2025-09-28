63cba124171bd586e2a21370f5ccbfd207c4d820
Nitesh47kumars <niteshshah028@gmail.com>
2025-09-28 23:36:40 +0530

    Play Video

--- src/Components/About/AboutVideo.jsx
+++ src/Components/About/AboutVideo.jsx
@@ -6,8 +6,10 @@ const AboutVideo = () => {
 
   return (
     <div className='overflow-hidden lg:w-[40%] relative -z-1'>
+      {/* Show skeleton while video is loading */}
       {!isVideoLoaded && <Skeleton />}
-{/* 
+
+      {/* Video container - always rendered */}
       <div className='w-full lg:h-120 max-lg:h-140 max-md:h-100 max-sm:h-100 object-cover overflow-hidden max-lg:translate-y-[-1rem]'>
         <video
           src='/video.mp4'
@@ -16,11 +18,14 @@ const AboutVideo = () => {
           muted
           autoPlay
           playsInline
-          onCanPlay={() => setIsVideoLoaded()}
-          className='h-full w-full object-cover rounded-2xl'
+          onCanPlay={() => setIsVideoLoaded(true)}
+          className={`h-full w-full object-cover rounded-2xl transition-opacity duration-500 ${
+            isVideoLoaded ? 'opacity-100' : 'opacity-0'
+          }`}
         />
-      </div> */}
+      </div>
 
+      {/* Overlay gradient */}
       <div className='absolute top-0 right-0 h-full w-full max-lg:bg-gradient-to-b lg:bg-gradient-to-r from-black to-transparent' />
     </div>
   );
