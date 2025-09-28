5927180e95ba788a34ef5060f93e7cb23171b8ff
Nitesh47kumars <niteshshah028@gmail.com>
2025-09-28 22:57:40 +0530

    Add Skeleton to AboutVideo

--- src/Components/About/AboutVideo.jsx
+++ src/Components/About/AboutVideo.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Skeleton from '../../UI/Skeleton';
 
 const AboutVideo = () => {
+  const [isVideoLoaded, setIsVideoLoaded] = useState(false);
   return (
     <div className='overflow-hidden lg:w-[40%] relative -z-1'>
+      {!isVideoLoaded && (
+        <Skeleton/>
+      )}
       <div className='w-full lg:h-120 max-lg:h-140 max-md:h-100 max-sm:h-100 object-cover overflow-hidden max-lg:translate-y-[-1rem]'>
         <video
           src='/video.mp4'
@@ -10,6 +15,7 @@ const AboutVideo = () => {
           muted
           autoPlay
           playsInline
+          onCanPlay={()=>setIsVideoLoaded(true)}
           className='h-full w-full object-cover rounded-2xl'
         />
       </div>
