d72246d6eca7b4a3b0d72504c1c8e410090f9719
Nitesh47kumars <niteshshah028@gmail.com>
2025-10-15 20:55:36 +0530

    Upload Latest Resume

--- src/Components/Navbar/NavbarBtn.jsx
+++ src/Components/Navbar/NavbarBtn.jsx
@@ -5,7 +5,7 @@ const NavbarBtn = () => {
   const [isHovered, setIsHovered] = useState(false);
 
   return (
-    <a href="/Resume.pdf" download
+    <a href="/Nitesh_Resume.pdf" download
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
       className="flex items-center gap-1 font-bold py-2 px-4 border-1 rounded-full bg-gradient-to-r from-[#8e2de2] to-[#ff6a00] hover:scale-105 transition-all duration-300
