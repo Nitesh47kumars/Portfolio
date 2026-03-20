bace59d800eb6ec593fc043325ac375d2a2773a6
Nitesh47kumars <niteshshah028@gmail.com>
2026-03-21 00:37:14 +0530

    updating

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
@@ -11,6 +10,6 @@ export default defineConfig({
 server:{
   host: true,
   port: '5173',
-  allowedHosts:['all','33f36ad52897.ngrok-free.app'],
+  allowedHosts:['all'],
 }
 })
